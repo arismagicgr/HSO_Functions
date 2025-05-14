09f8d4761186147082ba31d4a59972b057f614d2
A. Ares <arismagicgr@gmail.com>
2025-05-15 01:54:58 +0300

    Add files via upload

---
+++ alertTime.hpp
@@ -0,0 +1,6 @@
+class alertTime {
+  title = "How much time the group that will identify the player will need to make the call for the QRF";
+  texts[] = {"5s","10s","15s","20s","25s","30s","35s","40s","45s","50s","55s","60s","70s","80s","90s","100s","110s","120s"};
+  values[] = {5,10,15,20,25,30,35,40,45,50,55,60,70,80,90,100,110,120};
+  default = 15;
+};
---
+++ canCallQRF.hpp
@@ -0,0 +1,6 @@
+class canCallQRF {
+  title = "Will the QRF groups be able to call QRFs themselves? (May lead to a really high unit and group count for the mission)";
+  texts[] = {"No","Yes"};
+  values[] = {0,1};
+  default = 1;
+};
