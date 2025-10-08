3a5d89d5a80ea2a0e546d40cb9ad513a23e06fb2
A. Ares <arismagicgr@protonmail.com>
2025-10-09 01:02:38 +0300

    Update and rename canCallQRF.hpp to canCallQRFParam.hpp

--- Mission_Params/canCallQRFParam.hpp
+++ Mission_Params/canCallQRFParam.hpp
@@ -1,6 +1,7 @@
-class canCallQRF {
+class canCallQRFParam {
   title = "Will the QRF groups be able to call QRFs themselves? (May lead to a really high unit and group count for the mission)";
   texts[] = {"No","Yes"};
   values[] = {0,1};
   default = 1;
 };
+
